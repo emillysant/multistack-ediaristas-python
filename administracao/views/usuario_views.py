0a85a45e81f3d3b5d088bd7a94fbb6d602ab1703
Emilly Santiago <emillysantiago89@gmail.com>
2021-08-09 19:49:30 -0300

    Listagem de usuarios finalizada

---
+++ administracao/views/usuario_views.py
@@ -0,0 +1,17 @@
+from ..forms.usuario_forms import UsuarioForm
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UsuarioForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+    else:
+         form_usuario = UsuarioForm()
+    return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+def listar_usuarios(request):
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
